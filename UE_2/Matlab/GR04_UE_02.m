138c66251e35a9b506b92ae053491cdcf5db2e49
MatthiasFottner <matthiasfottner97@gmail.com>
2020-11-05 16:07:34 +0100

    merged matlab final version

--- UE_2/Matlab/GR04_UE_02.m
+++ UE_2/Matlab/GR04_UE_02.m
@@ -1,6 +1,7 @@
 clc
+Ohm = char(hex2dec('03A9'));
 
-%% Bauteilwerte
+%% Variablendefinition der bekannten Werte
 
 r1 = 5;
 r2 = 10;
@@ -20,7 +21,7 @@ g4 = 1/r4;
 g5 = 1/r5;
 g6 = 1/r6;
 
-%% Definition der Systemmatrix in der Form A * un = b
+%% Definition der Systemmatrix in der Form A * x = b
 
 A = [g1+g2, -g2, 0, 0, -g1, 0, -1;
     -g2, g2+g3+g4, 0, -g3, 0, 0, 0;
@@ -28,15 +29,65 @@ A = [g1+g2, -g2, 0, 0, -g1, 0, -1;
     0, -g3, -g5, g3+g5, 0, 1, 0;
     -g1, 0, 0, 0, g1, -1, 0;
     0, 0, 0, 1, -1, 0, 0;
-    -1, (-a*g3), 0, (a*g3), 0, 0, 0;]
-
-%InverseMatrix = A^(-1)
+    1, (a*g3), 0, -(a*g3), 0, 0, 0;];
 
 b = [0; -Is3; Is3; 0; 0; Us2; 0];
 
 %% Lösen der Systemgleichung
-un = A^(-1)*b
+x = A^(-1)*b
 
-%% Berechnung der Einzelspannungen und Einzelströme
-u = [un(5)-un(1);-un(2)+un(1);un(2)-un(3);un(1)-un(2);-un(2);-un(3)]
-i = u./[r1;r2;r3;r4;r5;r6]
+
+%% Berechnung von U_Th
+
+U_Th = x(4)-x(3)
+
+
+%% Berechnung von R_Th über den Kurzschlusstrom IKS: A_IKS * x_IKS = b_IKS
+
+A_IKS = [g1+g2, -g2, 0, 0, -g1, 0, -1, 0;
+    -g2, g2+g3+g4, 0, -g3, 0, 0, 0, 0;
+    0, 0, g5+g6, -g5, 0, 0, 0, -1;
+    0, -g3, -g5, g3+g5, 0, 1, 0, 1;
+    -g1, 0, 0, 0, g1, -1, 0, 0;
+    0, 0, 0, 1, -1, 0, 0, 0;
+    1, (a*g3), 0, -(a*g3), 0, 0, 0, 0;
+    0, 0, -1, 1, 0, 0 , 0, 0;];
+
+b_IKS = [0; -Is3; Is3; 0; 0; Us2; 0; 0];
+
+%% Lösen der Systemgleichung
+x_IKS = A_IKS^(-1)*b_IKS
+
+%% R_Th mithilfe von U_Th und I_KS
+
+R_Th_temp = U_Th/x_IKS(8);
+
+R_Th = sprintf('%.4f %s',R_Th_temp, Ohm)
+
+
+
+%% Berechnung von R_Th über die Testspannung U_T: A_T * x_T = b_T
+
+U_T = 1;
+
+A_T = [g1+g2, -g2, 0, 0, -g1, 0, -1, 0;
+    -g2, g2+g3+g4, 0, -g3, 0, 0, 0, 0;
+    0, 0, g5+g6, -g5, 0, 0, 0, -1;
+    0, -g3, -g5, g3+g5, 0, 1, 0, 1;
+    -g1, 0, 0, 0, g1, -1, 0, 0;
+    0, 0, 0, 1, -1, 0, 0, 0;
+    1, (a*g3), 0, -(a*g3), 0, 0, 0, 0;
+    0, 0, -1, 1, 0, 0 , 0, 0;];
+
+b_T = [0; 0; 0; 0; 0; 0; 0; U_T];
+
+%% Lösen der Systemgleichung und auslesen des Teststroms I_T?
+x_T_temp = A_T^(-1)*b_T;
+
+I_T = sprintf('%.4f A',x_T_temp(8))
+
+%% R_Th mithilfe von U_T und I_T
+
+R_Th_temp2 = -(U_T)/x_T_temp(8);
+
+R_Th = sprintf('%.4f %s',R_Th_temp2, Ohm)
