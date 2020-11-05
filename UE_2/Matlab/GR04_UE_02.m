b471a689482651d6dd2e667b1e37abf6dbb664ed
DavidKeller <keller.david.dc@gmail.com>
2020-11-05 11:33:54 +0100

    Update GR04_UE_02.m

--- UE_2/Matlab/GR04_UE_02.m
+++ UE_2/Matlab/GR04_UE_02.m
@@ -30,14 +30,8 @@ A = [g1+g2, -g2, 0, 0, -g1, 0, -1;
     0, 0, 0, 1, -1, 0, 0;
     -1, (-a*g3), 0, (a*g3), 0, 0, 0;]
 
-%InverseMatrix = A^(-1)
-
 b = [0; -Is3; Is3; 0; 0; Us2; 0];
 
 %% Lösen der Systemgleichung
 un = A^(-1)*b
-
-%% Berechnung der Einzelspannungen und Einzelströme
-u = [un(5)-un(1);-un(2)+un(1);un(2)-un(3);un(1)-un(2);-un(2);-un(3)]
-i = u./[r1;r2;r3;r4;r5;r6]
 
