4614fa49d0f46c7418d9b49df98dbf69f439d984
DavidKeller <keller.david.dc@gmail.com>
2020-11-18 17:36:56 +0100

    add figures

--- UE_4/Matlab/GR04_UE_04.m
+++ UE_4/Matlab/GR04_UE_04.m
@@ -1,3 +1,7 @@
+%Aufräumen der Matlab Umgebung.
+clc
+close all
+
 %% Variablendefinition der bekannten Werte
 
 r1 = 50;
@@ -11,46 +15,52 @@ I_L0 = 50 * 10^(-3);
 U_C0 = 25;
 tau_1 = L/r1;
 
-%% Bestimmen der Werte der Parameter ?, ?0 und ?d
+%% Bestimmen der Werte der Parameter delta, omega_0 und Omega_d
 
-delta = ((r2 * r3 * C + L)/(2*(r2 * L * C + r3 * L * C)));
-omega_0 = sqrt((r2)/(r2 * L * C + r3 * L * C));
-Omega_d = sqrt((omega_0^2 - delta^2));
+delta = ((r2 * r3 * C + L)/(2*(r2 * L * C + r3 * L * C)))
+omega_0 = sqrt((r2)/(r2 * L * C + r3 * L * C))
+Omega_d = sqrt((omega_0^2 - delta^2))
 
 %% Berechnung von i_L(t_tilde), K_1, K_2
 
-i_L_t_tilde = ((U_0-(U_0 - (r1 * I_L0)) * exp(-(2 * tau_1)/(tau_1)))/(r1));
+i_L_t_tilde = ((U_0-(U_0 - (r1 * I_L0)) * exp(-(2 * tau_1)/(tau_1)))/(r1))
 
-K_1 = i_L_t_tilde;
-
-%K_2_alt = (U_C0 - ((r2 * r3)/(r2 * L + r3 * L)) * i_L_t_tilde + delta * K_1)/(Omega_d);
-K_2_PHLIPP = (L * delta * K_1 + r3/r2 * L * delta * K_1 -U_C0 - r3 * K_1) / (L * Omega_d + L * Omega_d * r3 / r2);
+K_1 = i_L_t_tilde
 K_2 = ((delta * K_1 - (r2 * (U_C0 + r3 * i_L_t_tilde)) / (r2  * L + r3 * L)) / (Omega_d))
 
-%% Berechnung von i_L(t) und Plot
+%% Berechnung von i_L / u_L und Plot
 
-
+%Define time arrays
 t = linspace(0,0.02, 1000000);
 t_label = linspace(0,0.02, 11);
 A_label = linspace(-0.1,0.2, 7);
-A_label = sort([A_label, 0.1797])
+A_label = sort([A_label, 0.1797]);
 axLims = [0 0.02 -0.1 -0.2];  %[x-min, x-max, y-min, y-max] axis limits
-%t_label = [0, tau_1, 2*tau_1, 3*tau_1, 4*tau_1, 5*tau_1];
-%i_L_t((5*10^(-3)), delta, K_1, K_2, Omega_d, tau_1)
 
 
-%plot(t, (i_L_t_1(t, delta, K_1, K_2, Omega_d, tau_1)))
-figure('Name','Strom der Spule i_L','NumberTitle','off');
-plot(t, ((-1)*(heaviside(t-2*tau_1)-1)) .* i_L_t_0(t, U_0, r1, tau_1, I_L0) + heaviside(t-2*tau_1) .* (i_L_t_1(t, delta, K_1, K_2, Omega_d, tau_1)))
+% Plot of u_L
+figure('Name','Spannung der Spule','NumberTitle','off');
+plot(t, (((-1)*(heaviside(t-2*tau_1)-1)) .* u_L_t_1(t, U_0, r1, I_L0, tau_1, L) ...
+     + heaviside(t-2*tau_1) .* u_L_t_2(t, delta, K_1, K_2, Omega_d, L, tau_1)));
+xlabel('t in ms')
+ylabel('{u_L(t) in V}')
+
+xticks(t_label)
+xticklabels({'0','2','2 \tau_1','6','8','10', '12', '14', '16', '18', '20'})
+
+
+% Plot of i_L
+figure('Name','Strom der Spule','NumberTitle','off');
+plot(t, ((-1)*(heaviside(t-2*tau_1)-1)) .* i_L_t_0(t, U_0, r1, tau_1, I_L0) ...
+     + heaviside(t-2*tau_1) .* (i_L_t_1(t, delta, K_1, K_2, Omega_d, tau_1)));
 hold on
 point = [2*tau_1, 0.1797];
 plot(point(1), point(2), 'o')
 plot([point(1), point(1)], [axLims(3), point(2)], 'k:')  %vertical line
 plot([axLims(1), point(1)], [point(2), point(2)], 'k:')  %horizontal line
 
-
 xlabel('t in ms') 
-ylabel('mA') 
+ylabel('{i_L(t) in mA}') 
 
 xticks(t_label)
 xticklabels({'0','2','2 \tau_1','6','8','10', '12', '14', '16', '18', '20'})
@@ -58,8 +68,19 @@ yticks(A_label)
 yticklabels({'-100', '-50', '0', '50', '100', '150', '179.7', '200'})
 
 
+
+%% Functions of i_L / u_L 
 function i_L_t_1 = i_L_t_1(t, delta, K_1, K_2, Omega_d, tau_1)
     i_L_t_1 = exp(-((t-2*tau_1) * delta)) .* (K_1 * cos(Omega_d * (t-2*tau_1)) + K_2 * sin(Omega_d * (t-2*tau_1)));
+end
+
+function u_L_t_1 = u_L_t_1(t, U_0, r1, I_L0, tau_1, L)
+    u_L_t_1 = (U_0 / r1 + (U_0 - r1 * I_L0) / (r1 * tau_1) .* exp(-t / tau_1)) * L;
+end
+
+function u_L_t_2 = u_L_t_2(t, delta, K_1, K_2, Omega_d, L, tau_1)
+    t = (t-2*tau_1);
+    u_L_t_2 = L * (-delta * exp(-delta * t) .* (K_1 * cos(Omega_d * t) + K_2 * sin(Omega_d * t)) + exp(-delta * t) .* (-K_1 * Omega_d * sin(Omega_d * t) + K_2 * Omega_d * cos(Omega_d * t)));
 end
 
 function i_L_t_0 = i_L_t_0(t, U_0, r1, tau_1, I_L0)
--- UE_4/PSpice/whole_SCH.sch
+++ UE_4/PSpice/whole_SCH.sch
@@ -1,10 +1,11 @@
-*version 9.1 701064203
-u 86
+*version 9.1 3141332571
+u 88
 C? 2
 R? 4
 L? 2
 V? 2
 U? 4
+? 2
 @libraries
 @analysis
 .TRAN 1 0 0 0
@@ -23,11 +24,11 @@ connectViaLocalLabels 0
 NoStim4ExtIFPortsWarnings 1
 AutoGenStim4ExtIFPorts 1
 @index
-pageloc 1 0 3528 
+pageloc 1 0 3676 
 @status
 c 120:10:16:23:19:29;1605565169
 n 0 120:10:16:23:19:30;1605565170 e 
-s 2832 120:10:18:12:12:31;1605697951 e 
+s 2832 120:10:18:12:28:55;1605698935 e 
 *page 1 0 970 720 iA
 @ports
 port 47 GND_ANALOG 430 280 h
@@ -42,14 +43,6 @@ a 0 s 0:13 0 0 0 hln 100 PKGTYPE=
 a 0 a 0:13 0 0 0 hln 100 PKGREF=U3
 a 0 ap 9 0 0 20 hln 100 REFDES=U3
 a 0 u 13 13 -2 -4 hln 100 tClose=4ms
-part 6 L 240 150 d
-a 0 sp 0 0 0 10 hlb 100 PART=L
-a 0 s 0:13 0 0 0 hln 100 PKGTYPE=L2012C
-a 0 s 0:13 0 0 0 hln 100 GATE=
-a 0 u 13 0 21 47 hln 100 VALUE=100mH
-a 0 x 0:13 0 0 0 hln 100 PKGREF=L
-a 0 xp 9 0 15 0 hln 100 REFDES=L
-a 0 u 13 13 0 0 hln 100 IC=50mA
 part 4 r 310 180 v
 a 0 sp 0 0 0 10 hlb 100 PART=r
 a 0 s 0:13 0 0 0 hln 100 PKGTYPE=RC05
@@ -91,26 +84,28 @@ a 0 x 0:13 0 0 0 hln 100 PKGREF=C
 a 0 xp 9 0 15 0 hln 100 REFDES=C
 a 0 u 13 13 4 46 hln 100 IC=25V
 a 0 u 13 0 17 35 hln 100 VALUE=10uF
+part 6 L 240 150 d
+a 0 sp 0 0 0 10 hlb 100 PART=L
+a 0 s 0:13 0 0 0 hln 100 PKGTYPE=L2012C
+a 0 s 0:13 0 0 0 hln 100 GATE=
+a 0 u 13 0 21 47 hln 100 VALUE=100mH
+a 0 x 0:13 0 0 0 hln 100 PKGREF=L
+a 0 xp 9 0 15 0 hln 100 REFDES=L
+a 0 u 13 13 0 0 hln 100 IC=50mA
 part 1 titleblk 970 720 h
 a 1 s 13 0 350 10 hcn 100 PAGESIZE=A
 a 1 s 13 0 180 60 hcn 100 PAGETITLE=
 a 1 s 13 0 340 95 hrn 100 PAGECOUNT=1
 a 1 s 13 0 300 95 hrn 100 PAGENO=1
+part 87 iMarker 240 150 h
+a 0 s 0 0 0 0 hln 100 PROBEVAR=
+a 0 a 0 0 6 20 hlb 100 LABEL=1
 @conn
 w 41
 a 0 up 0:33 0 0 0 hln 100 V=
 s 430 120 460 120 40
 s 460 120 460 170 42
 a 0 up 33 0 462 145 hlt 100 V=
-w 71
-a 0 up 0:33 0 0 0 hln 100 V=
-s 240 120 330 120 22
-a 0 up 33 0 285 119 hct 100 V=
-s 330 120 390 120 28
-s 330 140 330 120 26
-s 360 140 330 140 24
-s 310 140 330 140 29
-s 240 150 240 120 74
 w 78
 a 0 up 0:33 0 0 0 hln 100 V=
 s 240 210 240 280 72
@@ -128,6 +123,15 @@ w 32
 a 0 up 0:33 0 0 0 hln 100 V=
 s 460 240 460 210 18
 a 0 up 33 0 500 209 hlt 100 V=
+w 71
+a 0 up 0:33 0 0 0 hln 100 V=
+s 240 120 330 120 22
+a 0 up 33 0 285 119 hct 100 V=
+s 330 120 390 120 28
+s 330 140 330 120 26
+s 360 140 330 140 24
+s 310 140 330 140 29
+s 240 150 240 120 74
 @junction
 j 310 180
 + p 10 2
@@ -191,6 +195,12 @@ j 310 140
 + w 71
 j 390 120
 + p 5 2
++ w 71
+j 240 150
++ p 87 pin1
++ p 6 1
+j 240 150
++ p 87 pin1
 + w 71
 @attributes
 a 0 s 0:13 0 0 0 hln 100 PAGETITLE=
